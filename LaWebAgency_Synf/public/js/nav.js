0393304318066d2dd2fdf438f578651320a56eae
Superelien <aurlat.simplon@gmail.com>
2019-11-03 12:22:14 +0100

    effet js misenp

--- LaWebAgency_Synf/public/js/nav.js
+++ LaWebAgency_Synf/public/js/nav.js
@@ -16,3 +16,30 @@ function toggleMenu() {
 }
 
 toggleMenu();
+
+// parallax
+var timeout;
+$('#container').mousemove(function(e){
+  if(timeout) clearTimeout(timeout);
+  setTimeout(callParallax.bind(null, e), 200);
+  
+});
+
+function callParallax(e){
+  parallaxIt(e, '.slide.one', -100);
+  parallaxIt(e, '.slide.two', -70);
+  parallaxIt(e, '.slide.three', -50);
+  parallaxIt(e, 'img', -30);
+}
+
+function parallaxIt(e, target, movement){
+  var $this = $('#container');
+  var relX = e.pageX - $this.offset().left;
+  var relY = e.pageY - $this.offset().top;
+  
+  TweenMax.to(target, 1, {
+    x: (relX - $this.width()/2) / $this.width() * movement,
+    y: (relY - $this.height()/2) / $this.height() * movement,
+    ease: Power2.easeOut
+  })
+}
